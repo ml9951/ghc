27290866f5ad53bbffe790c4a8acef09b5b2fd46
Matt Le <ml9951@rit.edu>
2015-07-16 13:08:13 -0400

    Adding partial abort and ordered stm

---
+++ libraries/pastm/Control/Ordered/CCode.c
@@ -0,0 +1,359 @@
+/* -----------------------------------------------------------------------------
+ * (c) The GHC Team 2015
+ *
+ * Partial Abort STM implementation.
+ *
+ * ---------------------------------------------------------------------------*/
+
+#include "PosixSource.h"
+#include "Rts.h"
+
+#include "RtsUtils.h"
+#include "Schedule.h"
+#include "PartialAbortSTM.h"
+#include "Trace.h"
+#include "Threads.h"
+#include "sm/Storage.h"
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define KBOUND 20
+#define START_FREQ 4
+
+//casts
+#define TO_WITHK(x) ((StgPTRecWithK*)x)
+#define TO_WITHOUTK(x) ((StgPTRecWithoutK*)x)
+#define TO_WRITE_SET(x) ((StgWriteSet*)x)
+#define TO_CLOSURE(x) ((StgClosure*)x)
+#define TO_OR_ELSE(x) ((StgPTRecOrElse *)x)
+
+static volatile unsigned long version_clock = 0;
+
+/*
+ * I think the header can be clean, since it isn't going to be pointing
+ * to anything in a younger generation.  I'm not sure how this interacts
+ * with the garbage collector, it would be nice if it never got moved...
+ */
+static StgTVar dummyTV = {.header = {.info = &stg_TVAR_CLEAN_info /*, TODO: add profiling field*/}, 
+                          .current_value = PASTM_SUCCESS, //dummy value
+                          .first_watch_queue_entry = ((StgTVarWatchQueue *)(void *)&stg_END_STM_WATCH_QUEUE_closure),
+                          .num_updates = 0};
+
+
+#define RECYCLE
+
+/*
+ * Allocate a new WithoutK item.  If we are recycling items, try
+ * and take one off of the capability.  If one doesn't exist, 
+ * allocate one.  Question: Should we separate the With and WithoutK
+ * items?  One option would be to put the entire read set on the 
+ * capability when a transaction commits or aborts, but only 
+ * recycle WithoutK items.  If we encounter a WithK item on the 
+ * recycled list, then we can simply overwrite its header.  
+ * Trying to recycle WithK items, would probably be too expensive
+ * as we would need to perform a linear search on the capability's 
+ * recycle list until we find one, unless we separate the two types.  
+ */
+StgPTRecWithoutK * newWithoutK(Capability * cap){
+    StgPTRecWithoutK * entry;
+#ifdef RECYCLE
+	StgPTRecWithoutK * free_list = cap->free_ptrec_items;
+	if(free_list != TO_WITHOUTK(NO_PTREC)){
+	    entry = free_list;
+		free_list = cap->free_ptrec_items = free_list->next;
+		SET_HDR(entry, &stg_PTREC_WITHOUTK_MUT_info, CCS_SYSTEM); //might be a WithK item
+		return entry;
+	}
+#endif	
+	entry = (StgPTRecWithoutK *)allocate(cap, sizeofW(StgPTRecWithoutK));
+	SET_HDR(ptrec, &stg_PTREC_WITHOUTK_MUT_info, CCS_SYSTEM);
+	
+	return entry;
+}
+
+
+StgPTRecHeader * pa_stmStartTransaction(Capability *cap) {
+    
+    StgPTRecHeader * ptrec;
+    ptrec = (StgPTRecHeader *)allocate(cap, sizeofW(StgPTRecHeader));
+    SET_HDR(ptrec , &stg_PTREC_HEADER_info, CCS_SYSTEM);
+    
+	StgPTRecWithoutK * firstEntry = newWithoutK(cap);
+	firstEntry->tvar = &dummyTV;
+	firstEntry->read_value = PASTM_SUCCESS;
+	firstEntry->next = TO_WITHOUTK(NO_PTREC);
+
+    ptrec->read_set = firstEntry;
+    ptrec->lastK = TO_WITHK(NO_PTREC);
+	ptrec->tail = firstEntry;
+    ptrec->write_set = TO_WRITE_SET(NO_PTREC);
+
+    ptrec->retry_stack = TO_OR_ELSE(NO_PTREC);
+
+    //get a read version
+    ptrec->read_version = version_clock;
+    while((ptrec->read_version & 1) != 0){
+        ptrec->read_version = version_clock;
+    }
+    ptrec->capture_freq = ((unsigned long)START_FREQ << 32) + START_FREQ ; 
+    ptrec->numK = 0;
+
+    return ptrec;
+}
+
+static StgPTRecWithK * pa_validate(StgPTRecHeader * trec){
+    while(TRUE){
+        unsigned long time = version_clock;
+        if((time & 1) != 0){
+            continue; //clock is locked
+        }
+        trec->read_version = time;
+        //validate read set
+        StgPTRecWithoutK * ptr = trec->read_set;
+        StgPTRecWithK *checkpoint = TO_WITHK(PASTM_SUCCESS);
+        StgInt kCount = 0;
+        while(ptr != TO_WITHOUTK(NO_PTREC)){
+            if(ptr->header.info == WITHK_HEADER){ // this is a WithK entry
+                StgPTRecWithK * withK = TO_WITHK(ptr);
+                if(withK->read_value != withK->tvar->current_value){
+                    checkpoint = TO_WITHK(PASTM_FAIL);
+                    ptr = ptr->next;
+                    kCount = 0;
+                    continue;
+                }else if(checkpoint == TO_WITHK(PASTM_FAIL)){ //Valid and we need a checkpoint
+                    checkpoint = withK;
+                }
+                kCount++;
+            }else if(ptr->read_value != ptr->tvar->current_value){  //WithoutK entry
+                checkpoint = TO_WITHK(PASTM_FAIL);
+                kCount = 0;
+            }
+            ptr = ptr->next;
+        }
+        if(checkpoint == TO_WITHK(PASTM_FAIL)){ //no checkpoint found, but we need to abort
+            return checkpoint;
+        }
+        if(checkpoint != TO_WITHK(PASTM_SUCCESS)){ //validation failed, but we found a checkpoint
+            //try reading from this tvar
+            StgTVar * tvar = checkpoint->tvar;
+            StgClosure * val = tvar->current_value;
+            if(time == version_clock){
+                checkpoint->read_value = val; //apply the continuation to this in C--
+                trec->read_set = TO_WITHOUTK(checkpoint);
+                trec->write_set = checkpoint->write_set;
+                trec->lastK = checkpoint;
+                StgInt64 capture_freq = trec->capture_freq & 0xFFFFFFFF00000000;
+                trec->capture_freq = capture_freq | (capture_freq >> 32);
+                trec->numK = kCount;
+                return checkpoint;
+            }else{
+                continue; //revalidate the whole log
+            }
+            
+        }
+
+        if(time == version_clock){
+            return checkpoint; //necessarily PASTM_SUCCESS
+        }
+        //Validation succeeded, but someone else committed in the meantime, loop back around...
+    }
+}
+
+StgClosure * pa_stmReadTVar(Capability * cap, StgPTRecHeader * trec, 
+                           StgTVar * tvar, StgClosure * k){
+    StgWriteSet * ws = trec->write_set;
+
+    while(ws != TO_WRITE_SET(NO_PTREC)){
+        if(ws->tvar == tvar){
+            return ws->val;
+        }
+        ws = ws->next;
+    }
+    
+    //Not found in write set
+    StgClosure * val = tvar->current_value;
+    while(trec->read_version != version_clock){
+        StgPTRecWithK * checkpoint = pa_validate(trec);
+        if(checkpoint != TO_WITHK(PASTM_SUCCESS)){
+            return TO_CLOSURE(checkpoint);
+        }
+        val = tvar->current_value;
+    }
+    
+    if(trec->numK < KBOUND){//Still room for more
+        if((trec->capture_freq & 0xFFFFFFFF) == 0){//Store the continuation
+            StgPTRecWithK * entry = (StgPTRecWithK *)allocate(cap, sizeofW(StgPTRecWithK));
+            SET_HDR(entry , &stg_PTREC_WITHK_info, CCS_SYSTEM);
+            entry->tvar = tvar;
+            entry->read_value = val;
+            entry->next = trec->read_set;
+            entry->write_set = trec->write_set;
+            entry->continuation = k;
+            entry->prev_k = trec->lastK;
+            entry->is_retry = FALSE;
+            trec->read_set = TO_WITHOUTK(entry);
+            trec->lastK = entry;
+            trec->numK++;
+            if(trec->numK == KBOUND)
+                trec->capture_freq <<= 1; //double the frequency
+            trec->capture_freq |= (trec->capture_freq >> 32);
+            
+        }else{//Don't store the continuation
+            StgPTRecWithoutK * entry = (StgPTRecWithoutK*)allocate(cap, sizeofW(StgPTRecWithoutK));
+            SET_HDR(entry, &stg_PTREC_WITHOUTK_info, CCS_SYSTEM);
+            entry->tvar = tvar;
+            entry->read_value = val;
+            entry->next = trec->read_set;
+            trec->read_set = entry;
+            trec->capture_freq--;
+        }
+    }else{//filter the read set
+        StgPTRecWithK * ptr = trec->lastK;
+        int numK = trec->numK;
+        while(ptr != TO_WITHK(NO_PTREC)){
+            if(ptr->prev_k != TO_WITHK(NO_PTREC)){
+                StgPTRecWithK * dropped = ptr->prev_k;
+                dropped->continuation = TO_CLOSURE(NO_PTREC);
+                ptr->prev_k = ptr->prev_k->prev_k;
+                ptr = ptr->prev_k;
+                SET_HDR(dropped, &stg_PTREC_WITHOUTK_info, CCS_SYSTEM);
+                numK--;
+            }else{
+                break;
+            }
+        }
+        trec->numK = numK;
+        StgPTRecWithoutK * entry = (StgPTRecWithoutK*)allocate(cap, sizeofW(StgPTRecWithoutK));
+        SET_HDR(entry, &stg_PTREC_WITHOUTK_info, CCS_SYSTEM);
+        entry->tvar = tvar;
+        entry->read_value = val;
+        entry->next = trec->read_set;
+        trec->read_set = entry;
+        trec->capture_freq--;
+    }
+    return val;
+}
+
+void pa_stmWriteTVar(Capability *cap,
+                    StgPTRecHeader *trec,
+                    StgTVar *tvar,
+                    StgClosure *new_value) {
+    StgWriteSet * newEntry = (StgWriteSet *) allocate(cap, sizeofW(StgWriteSet));
+    SET_HDR(newEntry , &stg_WRITE_SET_info, CCS_SYSTEM);
+    newEntry->tvar = tvar;
+    newEntry->val = new_value;
+    newEntry->next = trec->write_set;
+    trec->write_set = newEntry;
+}
+
+StgPTRecWithK * pa_stmCommitTransaction(Capability *cap, StgPTRecHeader *trec) {
+    unsigned long snapshot = trec->read_version;
+    while (cas(&version_clock, snapshot, snapshot+1) != snapshot){ 
+        StgPTRecWithK * checkpoint = pa_validate(trec);
+        if(checkpoint != (StgPTRecWithK *) PASTM_SUCCESS){
+            //The validate function sets up the trec with the appropriate read/write sets
+            return checkpoint;
+        }
+        snapshot = trec->read_version;
+    }
+    
+    StgWriteSet * write_set = trec->write_set;
+    stmPushWriteSet(cap, write_set);
+    while(write_set != TO_WRITE_SET(NO_PTREC)){
+        StgTVar * tvar = write_set->tvar;
+        tvar->current_value = write_set->val;
+        dirty_TVAR(cap,tvar);
+        write_set = write_set->next;
+    }
+
+    version_clock = snapshot + 2;//unlock clock
+    return (StgPTRecWithK *)PASTM_SUCCESS;
+}
+
+/* -----------------------------------------------------------------------------
+ * pa_setAtomicallyFrameHelper
+ * 
+ * This function is called by the preadTVar# primitive.  It traverses the stack
+ * leaving tso->sp referring to the enclosing atomically frame.  
+ * When performing an eager abort, we should only need to pop one frame...
+ * --------------------------------------------------------------------------- */
+void pa_setAtomicallyFrameHelper(Capability *cap, StgTSO *tso){
+  StgPtr           p, next;
+  StgRetInfoTable *info;
+
+  p = tso->stackobj->sp;
+  while (1) {
+    info = get_ret_itbl((StgClosure *)p);
+    next = p + stack_frame_sizeW((StgClosure *)p);
+    switch (info->i.type) {
+
+    case ATOMICALLY_FRAME:
+        tso->stackobj->sp = p;
+        return;
+
+    case UNDERFLOW_FRAME: 
+        tso->stackobj->sp = p;
+        threadStackUnderflow(cap,tso);
+        p = tso->stackobj->sp;
+        continue;
+
+    default:
+      ASSERT(info->i.type != CATCH_FRAME);
+      ASSERT(info->i.type != STOP_FRAME);
+      p = next;
+      continue;
+    }
+  }
+}
+
+/*
+ * Retry the transaction, we check to see that we 
+ * have something on our retry stack before entering
+ * this function.  This will return the alternative
+ * branch of the orElse and set the read set/write set
+ * appropriately.  
+ */
+StgClosure * pa_stmRetry(StgPTRecHeader * trec){
+    StgClosure * alt = trec->retry_stack->alt;
+    StgPTRecWithK * checkpoint = trec->retry_stack->read_set;
+    trec->read_set = checkpoint->next;               //set read set
+    trec->write_set = checkpoint->write_set;         //set write set
+    trec->lastK = checkpoint->prev_k;                //set short path
+    trec->retry_stack = trec->retry_stack->next;     //pop the retry stack
+
+    StgInt numK = 0;
+    StgPTRecWithK * ptr = checkpoint->prev_k;
+    while(ptr != TO_WITHK(NO_PTREC)){
+        numK++;
+        ptr = ptr->prev_k;
+    }
+
+    trec->numK = numK;
+    return alt;
+}
+
+void pa_stmCatchRetry(Capability *cap, StgPTRecHeader * trec, 
+                     StgClosure * alt, StgClosure * continuation){
+    StgPTRecWithK * checkpoint = (StgPTRecWithK*)allocate(cap, sizeofW(StgPTRecWithK));
+    SET_HDR(checkpoint, &stg_PTREC_WITHK_info, CCS_SYSTEM);
+    checkpoint->tvar = &dummyTV;
+    checkpoint->read_value = PASTM_SUCCESS;
+    checkpoint->write_set = trec->write_set;
+    checkpoint->next = trec->read_set;
+    checkpoint->continuation = continuation;
+    checkpoint->prev_k = trec->lastK;
+    checkpoint->is_retry = TRUE;
+    trec->read_set = TO_WITHOUTK(checkpoint);
+    trec->lastK = checkpoint;
+    //NOTE: don't update the continuation count or the capture frequency
+    
+    StgPTRecOrElse * orelse = (StgPTRecOrElse*)allocate(cap, sizeofW(StgPTRecOrElse));
+    SET_HDR(checkpoint, &stg_PTREC_OR_ELSE_info, CCS_SYSTEM);
+    orelse->alt = alt;
+    orelse->read_set = checkpoint;
+    orelse->next = trec->retry_stack;
+    trec->retry_stack = orelse;
+}
+
---
+++ libraries/pastm/Control/Ordered/STM.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE CPP, MagicHash, UnboxedTuples, Rank2Types, ForeignFunctionInterface, GHCForeignImportPrim, UnliftedFFITypes#-}
+
+#if __GLASGOW_HASKELL__ >= 701
+{-# LANGUAGE Trustworthy #-}
+#endif
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Full.STM
+-- Copyright   :  (c) The University of Glasgow 2004
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable (requires PASTM)
+--
+-- Software Transactional Memory: a modular composable concurrency
+-- abstraction.  See
+--
+--  * This contains the core STM operations
+--
+-----------------------------------------------------------------------------
+
+module Control.Partial.STM
+(
+    readTVar,
+    writeTVar,
+    atomically,
+    STM(..),
+  --  retry,
+  --  orElse,
+    --The following are just re-exporting from the original STM
+    newTVarIO,   
+    readTVarIO, 
+    TVar(..),    
+    newTVar       
+)
+where
+
+
+
+import GHC.Conc.Sync(TVar(..), readTVarIO, newTVarIO)
+import GHC.Base(State#, RealWorld, IO(..), ap, newTVar#, TVar#)
+import GHC.Prim(Any, unsafeCoerce# )
+
+newtype STM a = STM {unSTM :: forall r . --r is the type of the final result
+                                 (a -> State# RealWorld -> (# State# RealWorld, r #)) -> --Continuation
+                                 State# RealWorld ->                                     --Incoming State
+                                 (# State# RealWorld, r #)}                              --New state and result
+
+instance Monad STM where
+    return a = STM $ \c -> \s -> c a s
+    m >>= k = STM $ \c -> \s -> unSTM m (\a -> \s' -> unSTM (k a) c s') s
+
+instance Applicative STM where
+    (<*>) = ap
+    pure  = return
+
+instance  Functor STM where
+    fmap f m = m >>= (return . f)
+
+readTVar :: TVar a -> STM a
+readTVar (TVar tv) = STM $ \c -> \s-> case unsafeCoerce# readTVar# tv c s of
+                                        (# s', t #) -> c t s'
+
+
+writeTVar :: TVar a -> a -> STM ()
+writeTVar (TVar tv) a = STM $ \c -> \s -> 
+          case unsafeCoerce# writeTVar# tv a s of
+               s' -> c () s'
+
+newTVar :: a -> STM (TVar a)
+newTVar x = STM $ \c -> \s -> case newTVar# x s of
+                                (# s', tv #) -> c (TVar tv) s'
+
+initK :: a -> State# RealWorld -> (# State# RealWorld, a #)
+initK a s = (# s, a #)
+
+atomically :: STM a -> IO a
+atomically (STM c) = IO (\s -> unsafeCoerce# atomically# (c initK) s)
+
+{-
+-- |Retry execution of the current memory transaction because it has seen
+-- values in TVars which mean that it should not continue (e.g. the TVars
+-- represent a shared buffer that is now empty).  The implementation may
+-- block the thread until one of the TVars that it has read from has been
+-- udpated. (GHC only)
+retry :: STM a
+retry = STM $ \c -> \s# -> pretry# s#
+
+-- |Compose two alternative STM actions (GHC only).  If the first action
+-- completes without retrying then it forms the result of the orElse.
+-- Otherwise, if the first action retries, then the second action is
+-- tried in its place.  If both actions retry then the orElse as a
+-- whole retries.
+orElse :: STM a -> STM a -> STM a
+orElse (STM m) e = STM $ \c -> \s -> 
+       let m' = m c -- :: State# RealWorld -> (# State$ RealWorld, r #)
+       in pcatchRetry# m' (unSTM e c) (\a -> m') s
+-}
+
+foreign import prim "stg_partial_atomicallyzh" atomically# 
+        :: Any() -> State# s -> (# State# s, Any() #)
+{-         FFI won't accept this type...
+        :: (State# RealWorld -> (# State# RealWorld , b #) )
+            -> State# RealWorld -> (# State# RealWorld, b #)
+-} 
+
+foreign import prim "stg_partial_readTVarzh" readTVar#
+        :: TVar# s a -> Any()
+            -> State# s -> (# State# s, a #)
+
+foreign import prim "stg_partial_writeTVarzh" writeTVar#
+        :: TVar# RealWorld a -> Any() -> State# RealWorld -> (# State# RealWorld, TVar# RealWorld a #)
+
+
