318ce8fd0b8d3f6befe0de91f0bdb758e6c9c6b8
Matt Le <ml9951@rit.edu>
2015-07-24 18:10:26 -0400

    Adding stm files

---
+++ libraries/pastm/Control/STMHaskell/STM.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE CPP, MagicHash, UnboxedTuples, ForeignFunctionInterface #-}
+
+#if __GLASGOW_HASKELL__ >= 701
+{-# LANGUAGE Trustworthy #-}
+#endif
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Concurrent.STM.TVar
+-- Copyright   :  (c) The University of Glasgow 2004
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable (requires STM)
+--
+-- TVar: Transactional variables
+--
+-----------------------------------------------------------------------------
+
+module Control.STMHaskell.STM (
+        -- * TVars
+        STM,
+        TVar,
+        atomically,
+        newTVar,
+        newTVarIO,
+        readTVar,
+        readTVarIO,
+        writeTVar,
+        printStats
+  ) where
+
+#ifdef __GLASGOW_HASKELL__
+import GHC.Base
+import GHC.Conc
+#else
+import Control.Sequential.STM
+#endif
+
+foreign import ccall "stmPrintStats" printStats :: IO()
+
+
+
+
