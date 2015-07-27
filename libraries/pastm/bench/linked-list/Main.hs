c1b907e092be1173043a8ea5ab452f68b16f0c89
Matt Le <ml9951@rit.edu>
2015-07-27 14:10:47 -0400

    Adding linked list benchmark

---
+++ libraries/pastm/bench/linked-list/Main.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Prelude hiding (reads)
+import GHC.Conc(numCapabilities)
+import Options.Applicative
+import LinkedList
+import Control.Monad.Trans.State
+import System.Random
+import Control.Concurrent.MVar
+import Control.Concurrent(forkOn)
+import Text.Printf
+
+data Opts = Opts
+     { iters     :: Int
+     , threads   :: Int
+     , maxVal    :: Int
+     , initSize  :: Int
+     , reads     :: Int
+     , writes    :: Int
+     , deletes   :: Int
+     } deriving Show
+
+{-
+parser :: String -> Opts
+parser prog = Opts
+       { iters      = 3000
+                   &= help "Number of operations performed by each thread"
+       , threads    = getNumCapabilities
+                   &= help "Number of threads"
+       , maxVal     = 100000
+                   &= help "Largest value to be added to the list"
+       , initSize   = 4000
+                   &= help "Initial size of the linked list"
+       , reads      = 2
+                   &= help "Read proportion"
+       , writes     = 4
+                   &= help "Write proportion"
+       , deletes    = 1
+                   &= help "Delete proportion"
+       }
+       &= program prog
+-}
+parser :: Parser Opts
+parser = Opts
+      <$> option auto (short 'i' <> help "Number of operations performed by each thread" <> value 3000)
+      <*> option auto (short 't' <> help "Number of threads" <> value numCapabilities)
+      <*> option auto (long "maxVal" <> help "Max value to be added to the list" <> value 10000000)
+      <*> option auto (long "initSize" <> help "Initial size of the linked list" <> value 4000)
+      <*> option auto (long "reads" <> help "Read proportion" <> value 2)
+      <*> option auto (long "writes" <> help "Write proportion" <> value 4)
+      <*> option auto (long "deletes" <> help "Delete proportion" <> value 1)
+
+opts = info (helper  <*> parser) fullDesc
+
+
+
+threadLoop l opts 0 rands = return()
+threadLoop l opts i (r:rands)= 
+           let sum = reads opts + writes opts + deletes opts
+               prob = r `mod` sum
+           in 
+              if prob < reads opts
+              then find l r >>= \_ -> threadLoop l opts (i-1) rands
+              else if prob < reads opts + writes opts
+                   then add l r >>= \_ -> threadLoop l opts (i-1) rands
+                   else do 
+                        size <- getSize l
+                        delete l (r `mod` size)
+                        threadLoop l opts (i-1) rands
+
+
+
+mkThreads 0 l opts = return[]
+mkThreads i l opts = do
+          mv <- newEmptyMVar
+          forkOn (i-1) $ do threadLoop l opts (iters opts) (randoms (mkStdGen i)); putMVar mv()
+          mvs <- mkThreads (i-1) l opts
+          return $ mv : mvs
+
+join :: [MVar()] -> IO ()
+join [] = return()
+join (mv:mvs) = do
+     takeMVar mv
+     join mvs
+
+
+main = do
+     opts <- execParser opts
+     print opts
+     l <- newList
+     start <- getTime
+     mvars <- mkThreads (threads opts) l opts
+     join mvars
+     end <- getTime
+     printf "Time = %0.3f\n" (end - start :: Double)
+     printStats
+     return()
+
+foreign import ccall unsafe "hs_gettime" getTime :: IO Double
+
---
+++ libraries/pastm/bench/linked-list/ordered-linked-list.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE CPP #-} 
+module LinkedList(newList, add, find, delete, deleteIndex, ListHandle, getSize, printStats)
+where
+
+#ifdef STMHASKELL
+import Control.STMHaskell.STM     --full abort STM haskell
+#elif defined(FABORT)
+import Control.Full.STM           --full abort STM (NoRec)
+#elif defined(ORDERED)
+import Control.Ordered.STM
+#elif defined(TL2)
+import Control.PartialTL2.STM
+#else
+import Control.Partial.STM
+#endif
+
+data List a = Node {val :: a, next :: (TVar (List a))}
+            | Null
+            | Head {next :: (TVar (List a))}
+
+data ListHandle a = ListHandle{hd :: TVar (List a), size :: TVar Int}
+
+getSize (ListHandle{hd=hd,size=size}) = atomically $ readTVar size
+
+newList = atomically $ do
+        l <- newTVar Null
+        count <- newTVar 0
+        return (ListHandle{hd=l, size=count})
+
+inc size n = do
+    x <- readTVar size
+    writeTVar size (x+n)
+
+add :: Ord a => ListHandle a -> a -> IO()
+add (ListHandle{hd=hd, size=size}) v = do atomically (addLoop hd); atomically (inc size 1)
+    where
+    addLoop l = do
+            raw <- readTVar l
+            case raw of
+                 Head n -> addLoop n
+                 Null -> do
+                      newNull <- newTVar Null
+                      writeTVar l (Node{val=v,next=newNull})
+                 Node v' n -> 
+                      if v > v'
+                      then addLoop n
+                      else do
+                           newNode <- newTVar (Node v' n)
+                           writeTVar l (Node {val=v, next=newNode})
+
+find :: Eq a => ListHandle a -> a -> IO Bool
+find (ListHandle{hd=hd,size=size}) v = atomically (findLoop hd)
+     where
+     findLoop l = do
+              raw <- readTVar l
+              case raw of
+                   Head n -> findLoop n
+                   Null -> return(False)
+                   Node{val=v',next=n} ->
+                                      if v == v'
+                                      then return True
+                                      else findLoop n
+
+delete :: Ord a => ListHandle a -> a -> IO Bool
+delete (ListHandle{hd=hd,size=size}) v = do 
+       res <- atomically (readTVar hd >>= \raw -> deleteLoop (next raw) hd); 
+       if res
+       then atomically (inc size (-1)) >>= \_ -> return res
+       else return res
+       where
+       deleteLoop l prev = do
+                  raw <- readTVar l
+                  case raw of
+                       Null -> return False
+                       Node{val=v',next=n} ->
+                                           if v /= v'
+                                           then deleteLoop n l
+                                           else do
+                                                rawPrev <- readTVar prev
+                                                case rawPrev of
+                                                     Head n -> writeTVar prev (Head n) >>= \_ -> return True
+                                                     Node{val=v'',next=n'} -> writeTVar prev (Node{val=v'',next=n}) >>= \_ -> return True
+                                                     
+
+deleteIndex (ListHandle{hd=hd,size=size}) v = do
+            res <- atomically (readTVar hd >>= \raw -> deleteLoop (next raw) hd v)      
+            if res 
+            then atomically (inc size (-1)) >>= \_ -> return res
+            else return res
+            where
+            deleteLoop l prev 0 = do
+                       raw <- readTVar l 
+                       prevRaw <- readTVar prev
+                       case prevRaw of
+                            Head n -> writeTVar prev (Head (next raw)) >>= \_ -> return True
+                            Node{val=v',next=n} -> writeTVar prev (Node{val=v',next=next raw}) >>= \_ -> return True
+            deleteLoop l prev i = do
+                       raw <- readTVar l
+                       case raw of
+                            Head n -> deleteLoop n l (i-1)
+                            Node{next=n} -> deleteLoop n l (i-1)
+                            Null -> return False
+                       
+
