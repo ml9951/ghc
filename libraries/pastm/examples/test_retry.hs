6410f6c21b140d194db3e748b7bc444a89d7dd2a
Matt Le <ml9951@rit.edu>
2015-07-29 16:03:24 -0400

    Updating test_retry

--- libraries/pastm/examples/test_retry.hs
+++ libraries/pastm/examples/test_retry.hs
@@ -4,7 +4,7 @@ import Dump
 
 
 main = do
-     x <- atomically $ (((traceM "returning 1" >>= \_ -> return 1) `orElse` (traceM "returning 2" >>= \_ -> return 2)) >>= \t -> if t == 1 then (traceM ("retrying, t = " ++ show t) >>= \_ -> retry) else return t) `orElse` return 3
+     x <- atomically $ ((return 1 `orElse` return 2) >>= \t -> if t == 1 then retry else return t) `orElse` return 3
      putStrLn("Result = " ++ show x ++ ", should be 3")
      return()
 
